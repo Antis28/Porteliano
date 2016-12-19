d7801f09a7456b1ef52244b157e2e58e4e6444d2
Antis <antis.smith@gmail.com>
2016-12-19 03:18:25 +0300

    feature(mobile)(wishlist)

--- advanced/frontend/views/cart/13_Wishlist.php
+++ advanced/frontend/views/cart/13_Wishlist.php
@@ -17,17 +17,17 @@ use app\assets\CartAsset;
 use app\assets\BasketAsset;
 $countGoods = '2 товара';
 BasketAsset::register($this);
-//CartAsset::register($this);
+CartAsset::register($this);
 Yii::setAlias('@img', '@web/img/');
 
 ?>
 <div class="wrap-wishlist">
     <!-- заголовок -->
-    <div class="row">
-        <div class="col-md-2 ">
-            <h1>Избранное</h1>
+    <div class="row center-block">
+        <div class="col-md-2 col-xs-2">
+            <h1 class="wishlist-header">Избранное</h1>
         </div>
-        <div class="col-md-10 ">
+        <div class="col-md-10 col-xs-10">
             <h4 id="count-goods"><span id="counter-goods">
                     <?= $wish->getQuantity() ?> </span><span class="word-product"> товара</span></h4>
         </div>
@@ -44,12 +44,12 @@ Yii::setAlias('@img', '@web/img/');
 
     <div class="row goods-row">
         <!-- иконка -->
-        <div class="col-md-1  wishlist-icon">
+        <div class="col-md-1 col-xs-1   wishlist-icon">
             <?= Html::img($item['product']->getImage(),
                 ['class' => 'door-icon', 'alt' => "door icon"]) ?>
         </div>
         <!-- описание и удаление -->
-        <div class="col-md-4 wishlist-descript">
+        <div class="col-md-4 col-xs-4 wishlist-descript">
             <!-- описание -->
             <div class="row">
                 <div class="col-md-12">
@@ -68,7 +68,7 @@ Yii::setAlias('@img', '@web/img/');
             </div>
         </div>
         <!-- кнопка в корзину -->
-        <div class="col-md-1 button-area">
+        <div class="col-md-1 col-xs-1 button-area">
             <a href="#"  onclick="addToCart(event)">
                 <span data-id="<?= $item['product']->id ?> "
                     class="glyphicon glyphicon-shopping-cart btn btn-default basket-button"
